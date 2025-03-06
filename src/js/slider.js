e92146526c0b90fabb1dfbcd6cc112c9a98f4b49
Daria Nastas <bycinska@gmail.com>
2025-03-06 16:59:29 +0200

    add task solution

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,29 +1,30 @@
+'use strict';
+
+const images = [
+  'https://surl.li/htxzej',
+  'https://surl.li/ugxvhv',
+  'https://surl.gd/fhgope',
+];
+
 document.addEventListener("DOMContentLoaded", () => {
-  const sliderImage = document.getElementById('slider__image');
+  const sliderImage = document.querySelector('.slider__image');
   const moveBack = document.querySelector('.slider__arrow-btn--left');
   const moveForward = document.querySelector('.slider__arrow-btn--right');
-
-  const images = [
-    '../image/slider/slide-img-main.jpg',
-    '../image/slider/slide-img-2.jpg',
-    '../image/slider/slide-img-3.jpg',
-  ];
-
+  console.log(sliderImage)
+ 
   let index = 0;
-
   function updateImage() {
     sliderImage.style.backgroundImage = `url(${images[index]})`;
   }
-
   moveBack.addEventListener("click", () => {
     index =  (index - 1 + images.length) % images.length;
-    updateImage(index);
+    updateImage();
+    console.log(1)
   });
-
   moveForward.addEventListener("click", () => {
-    index = (index + 1) % index.length;
-    updateImage(index);
+    index = (index + 1) % images.length;
+    updateImage();
+    console.log(2)
   });
-
   updateImage(index);
 });
